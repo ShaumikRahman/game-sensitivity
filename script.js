f200fc73faf1074f4da68fb45a52d27a085c72cc
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2020-12-16 17:06:04 +0000

    saved sens and game

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let sens = 0;
+let game;
 
 let sensInputs = document.getElementsByClassName('sensInput');
 for (let i = 0; i < sensInputs.length; i++) {
     sensInputs[i].addEventListener('input', e => {
-        console.log(e.target.value);
+        sens = e.target.value;
+        game = e.target.id;
+        conversion(sens);
+        //console.log(e.target.id);
     })
+}
+
+function conversion(sens) {
+
 }
