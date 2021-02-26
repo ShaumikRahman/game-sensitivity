bfdfc7491c257ee48b2de58e63cdd89c68c86b5f
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-02-26 20:46:36 +0000

    conversion and autofill

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 let sens = 0;
 let game;
+let csgoMultiplier = new Map();
+let sensList = new Map();
+
+csgoMultiplier.set('overwatchSens', 3.33);
+csgoMultiplier.set('valorantSens', 0.314);
+csgoMultiplier.set('siegeSens', 0.001536);
 
 let sensInputs = document.getElementsByClassName('sensInput');
 for (let i = 0; i < sensInputs.length; i++) {
     sensInputs[i].addEventListener('input', e => {
         sens = e.target.value;
         game = e.target.id;
-        conversion(sens);
-        //console.log(e.target.id);
+        console.log(`Input is ${sens} from ${game}`);
+        conversion();
+        sensList.forEach(setNewSens)
     })
 }
 
-function conversion(sens) {
+function conversion() {
+    if (game == 'csgoSens') {
+        sensList.set('csgoSens', sens);
+        sensList.set('overwatchSens', 3.33 * sens);
+        sensList.set('valorantSens', 0.314 * sens);
+        sensList.set('siegeSens', 0.001536 * sens);
+    } else {
+        csgoMultiplier.forEach(
+            function temp (value, key) {
+                if (game == key) {
+                    sens = sens / value;
+                    game = 'csgoSens';
+                    conversion(sens);
+                }
+            }
+        );
+    }
+}
 
+function setNewSens(value, key, map) {
+    document.getElementById(key).value = value;
+    console.log(`Key is ${key} and value is ${value}`);
 }
