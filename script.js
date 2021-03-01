627d73ac277fd6c10a2ac40f38511c4627dffa8c
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-03-01 17:43:46 +0000

    added more games, changed box width

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let sensList = new Map();
 csgoMultiplier.set('overwatchSens', 3.33);
 csgoMultiplier.set('valorantSens', 0.314);
 csgoMultiplier.set('siegeSens', 0.001536);
+csgoMultiplier.set('warzoneSens', 3.33333);
+csgoMultiplier.set('coldWarSens', 3.33);
+csgoMultiplier.set('apexSens', 1.00);
+csgoMultiplier.set('fortniteSens', 3.96);
+csgoMultiplier.set('tarkovSens', 0.176000);
+csgoMultiplier.set('rustSens', 1.9555556);
 
 let sensInputs = document.getElementsByClassName('sensInput');
 for (let i = 0; i < sensInputs.length; i++) {
@@ -14,6 +20,7 @@ for (let i = 0; i < sensInputs.length; i++) {
         game = e.target.id;
         console.log(`Input is ${sens} from ${game}`);
         conversion();
+        //setTimeout(sensList.forEach(setNewSens), 1000);
         sensList.forEach(setNewSens)
     })
 }
@@ -24,6 +31,12 @@ function conversion() {
         sensList.set('overwatchSens', 3.33 * sens);
         sensList.set('valorantSens', 0.314 * sens);
         sensList.set('siegeSens', 0.001536 * sens);
+        sensList.set('warzoneSens', 3.33333 * sens);
+        sensList.set('coldWarSens', 3.33 * sens);
+        sensList.set('apexSens', 1.00 * sens);
+        sensList.set('fortniteSens', 3.96 * sens);
+        sensList.set('tarkovSens', 0.176000 * sens);
+        sensList.set('rustSens', 1.9555556 * sens);
     } else {
         csgoMultiplier.forEach(
             function temp (value, key) {
